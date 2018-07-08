32a039f03592bddad83f41a21cf0520b90324411
Oliver Rausch <oliverrausch99@gmail.com>
2018-07-08 23:00:56 +0200

    Add estimator

--- sklearn/semi_supervised/self_training_dev.py
+++ sklearn/semi_supervised/self_training_dev.py
@@ -1,5 +1,6 @@
 import numpy as np
 from sklearn.utils import shuffle
+from sklearn.neighbors import KNeighborsClassifier
 from sklearn.datasets import load_iris
 
 X, y = load_iris(return_X_y=True)
@@ -9,5 +10,9 @@ X, y = shuffle(X,y, random_state=42)
 #y = y[:30]
 
 y[30:] = -1
+
+estimator = KNeighborsClassifier()
+
+estimator.fit(X[:30],y[:30])
 
 print(y)
