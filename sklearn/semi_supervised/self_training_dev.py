70a2da6a233b115c4a608cf6a04d93c108f4131e
pr0duktiv <patrice@5becker.de>
2018-07-10 01:25:23 +0200

    Plot f1 scores to compare graphs. Potential error

--- sklearn/semi_supervised/self_training_dev.py
+++ sklearn/semi_supervised/self_training_dev.py
@@ -1,5 +1,5 @@
 import numpy as np
-import matplotlib as plt
+import matplotlib.pyplot as plt
 from self_training import SelfTraining
 from sklearn.utils import shuffle
 from sklearn.neighbors import KNeighborsClassifier
@@ -8,37 +8,33 @@ from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import StratifiedKFold
 from sklearn.metrics import accuracy_score, f1_score, roc_auc_score
 
-lim = 150
+est_score = []
+st_score = []
 
-X, y = load_breast_cancer(return_X_y=True)
-X, y_testreal = load_breast_cancer(return_X_y=True)
-X, y, y_testreal = shuffle(X,y, y_testreal, random_state=42)
-y[lim:] = -1
+for i in range(20,250):
+    lim = i
 
-def get_metrics_filter(estimator):
-    skfolds = StratifiedKFold(n_splits=4, random_state=42)
-    for train_index, test_index in skfolds.split(X,y):
-        X_train = X[train_index]
-        y_train = y[train_index]
-        X_test = X[test_index]
-        y_test = y[test_index]
-        y_test_true = y_testreal[test_index]
-        X_train_filtered = X_train[np.where(y_train != -1)]
-        y_train_filtered = y_train[np.where(y_train != -1)]
-        print(y_train_filtered)
+    X, y = load_breast_cancer(return_X_y=True)
+    X, y_testreal = load_breast_cancer(return_X_y=True)
+    X, y, y_testreal = shuffle(X,y, y_testreal, random_state=42)
+    y[lim:] = -1
 
-        estimator.fit(X_train_filtered, y_train_filtered)
-        y_pred = estimator.predict(X_test)
-        print(accuracy_score(y_pred, y_test_true))
+    est = KNeighborsClassifier()
+    #get_metrics_filter(est)
+    est.fit(X[:lim],y[:lim])
+    pred = est.predict(X[lim:]).round()
+    est_score.append(f1_score(pred, y_testreal[lim:]))
+    #print('Supervised Accucary: %f' % accuracy_score(pred, y_testreal[lim:]))
 
-est = KNeighborsClassifier()
-#get_metrics_filter(est)
-est.fit(X[:lim],y[:lim])
-pred = est.predict(X[lim:]).round()
-print('Supervised Accucary: %f' % accuracy_score(pred, y_testreal[lim:]))
+    st = SelfTraining(est, u=15, k=20)
+    st.fit(X, y)
+    pred = st.predict(X[lim:]).round()
+    st_score.append(f1_score(pred, y_testreal[lim:]))
+    #print('Self Training Accucary: %f' % accuracy_score(pred, y_testreal[lim:]))
 
-st = SelfTraining(est)
-st.fit(X, y)
-pred = st.predict(X[lim:]).round()
-print('Self Training Accucary: %f' % accuracy_score(pred, y_testreal[lim:]))
+plt.figure(1)
+plt.plot(est_score, label='Supervised')
+plt.plot(st_score, label='Semisupervised')
+plt.legend()
+plt.show()
 
