e97375115e4969090cc042348653fb0f79306661
pr0duktiv <patrice@5becker.de>
2018-07-12 00:38:32 +0200

    Desperate attemps to get good scores

--- sklearn/semi_supervised/self_training_dev.py
+++ sklearn/semi_supervised/self_training_dev.py
@@ -19,13 +19,13 @@ parameters = {'u':[8,10,12,14,16,18,20], 'k':[4,6,8,10,12,14,16]}
 
 X, y = load_breast_cancer(return_X_y=True)
 
-for t in tqdm(range(42,52)):
+for t in tqdm(range(42,62)):
     est_score = []
     st_score = []
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=t)
 
-    for i in range(10,50):
+    for i in range(10,80):
         lim = i
 
         y_train_st = y_train.copy()
@@ -37,17 +37,28 @@ for t in tqdm(range(42,52)):
         est_score.append(f1_score(pred, y_test))
         #print('Supervised Accucary: %f' % accuracy_score(pred, y_testreal[lim:]))
 
-        grid = []
-        for k in [4,6,8,10,12,14,16]:
-            for u in [8,10,12,14,16,18,20]:
-                st = SelfTraining(est, u=u, k=k)
-                #st = GridSearchCV(st, parameters, 'accuracy')
-                st.fit(X_train, y_train_st)
-                pred = st.predict(X_test).round()
-                grid.append(f1_score(pred, y_test))
-                #print('Self Training Accucary: %f' % accuracy_score(pred, y_testreal[lim:]))
+        #grid = -1
+        #best_p = (-1,-1)
 
-        st_score.append(max(grid))
+        #for k in [4,6,8,10,12,14,16]:
+        #    for u in [8,10,12,14,16,18,20]:
+        #        st = SelfTraining(est, u=u, k=k)
+        #        #st = GridSearchCV(st, parameters, 'accuracy')
+        #        st.fit(X_train, y_train_st)
+        #        pred = st.predict(X_test).round()
+        #        score = f1_score(pred, y_test)
+        #        if (score > grid):
+        #            best_p = (u,k)
+        #            grid = score
+        #        #print('Self Training Accucary: %f' % accuracy_score(pred, y_testreal[lim:]))
+        #print(best_p)
+        #st_score.append(grid)
+
+        st = SelfTraining(est, u=10, k=20)
+        #st = GridSearchCV(st, parameters, 'accuracy')
+        st.fit(X_train, y_train_st)
+        pred = st.predict(X_test).round()
+        st_score.append(f1_score(pred, y_test))
 
     est_score_set.append(est_score)
     st_score_set.append(st_score)
