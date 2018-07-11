aa0213685a399812145e206aed3b282d59a06689
pr0duktiv <patrice@5becker.de>
2018-07-11 23:53:49 +0200

    Implement manual cross validation (temp)

--- sklearn/semi_supervised/self_training_dev.py
+++ sklearn/semi_supervised/self_training_dev.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from tqdm import tqdm
 from self_training import SelfTraining
 from sklearn.utils import shuffle
 from sklearn.neighbors import KNeighborsClassifier
@@ -8,34 +9,53 @@ from sklearn.model_selection import cross_val_score, train_test_split
 from sklearn.model_selection import StratifiedKFold
 from sklearn.metrics import accuracy_score, f1_score, roc_auc_score
 
-est_score = []
-st_score = []
+est_score_set = []
+final_est = []
+
+st_score_set = []
+final_st = []
 
 X, y = load_breast_cancer(return_X_y=True)
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 
+for t in tqdm(range(42,52)):
+    est_score = []
+    st_score = []
 
-for i in range(10,50):
-    lim = i
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=t)
 
-    y_train_st = y_train.copy()
-    y_train_st[lim:] = -1
+    for i in range(10,50):
+        lim = i
 
-    est = KNeighborsClassifier()
-    est.fit(X_train[:lim],y_train[:lim])
-    pred = est.predict(X_test).round()
-    est_score.append(f1_score(pred, y_test))
-    #print('Supervised Accucary: %f' % accuracy_score(pred, y_testreal[lim:]))
+        y_train_st = y_train.copy()
+        y_train_st[lim:] = -1
 
-    st = SelfTraining(est, u=15, k=20)
-    st.fit(X_train, y_train_st)
-    pred = st.predict(X_test).round()
-    st_score.append(f1_score(pred, y_test))
-    #print('Self Training Accucary: %f' % accuracy_score(pred, y_testreal[lim:]))
+        est = KNeighborsClassifier()
+        est.fit(X_train[:lim],y_train[:lim])
+        pred = est.predict(X_test).round()
+        est_score.append(f1_score(pred, y_test))
+        #print('Supervised Accucary: %f' % accuracy_score(pred, y_testreal[lim:]))
+
+        st = SelfTraining(est, u=15, k=10)
+        st.fit(X_train, y_train_st)
+        pred = st.predict(X_test).round()
+        st_score.append(f1_score(pred, y_test))
+        #print('Self Training Accucary: %f' % accuracy_score(pred, y_testreal[lim:]))
+
+    est_score_set.append(est_score)
+    st_score_set.append(st_score)
+
+est_score_set = np.array(est_score_set).T.tolist()
+for l in est_score_set:
+    final_est.append(np.mean(l))
+
+st_score_set = np.array(st_score_set).T.tolist()
+for l in st_score_set:
+    final_st.append(np.mean(l))
+
 
 plt.figure(1)
-plt.plot(est_score, label='Supervised')
-plt.plot(st_score, label='Semisupervised')
+plt.plot(final_est, label='Supervised')
+plt.plot(final_st, label='Semisupervised')
 plt.legend()
 plt.show()
 
